16a4c45785705ff4152d72466a16c42c14ce6819
HieuDT2108 <79426353+HieuDT2108@users.noreply.github.com>
2024-05-28 08:11:01 +0700

    Add files via upload

---
+++ adc_driver.h
@@ -0,0 +1,53 @@
+/*
+ * adc_driver.h
+ *
+ *  Created on: Jan 8, 2024
+ *      Author: Le Nguyen Van Duong
+ */
+
+#ifndef SOURCES_ADC_DRIVER_ADC_DRIVER_H_
+#define SOURCES_ADC_DRIVER_ADC_DRIVER_H_
+
+/************************************************************************************
+ * Include file
+ ************************************************************************************/
+#include "adc_hal.h"
+#include "gpio_driver.h"
+
+/* Enumeration for possible states of the ADC driver */
+typedef enum
+{
+	ADC_DRV_OK,   	/* Successful ADC driver operation */
+	ADC_DRV_ERROR,	/* ADC driver encountered an error */
+
+}ADC_DRV_State;
+/* Structure defining the configuration parameters for ADC0 */
+typedef struct
+{
+    gpio_name           PortName;			 /* GPIO port name for ADC pin */
+    uint32_t            PinName; 		     /* GPIO pin number for ADC pin */
+    Adc0ConverterConfig ConverterConfig;	 /* Configuration for ADC0 converter */
+
+}Adc0_config;
+
+/************************************************************************************
+ * Prototypes
+ ************************************************************************************/
+
+/*************************************************************************************
+  * Function Name:		ADC0_DRV_Init
+  * Description:		Initializes the ADC driver for ADC0 based on the provided configuration.
+  * @param adc0_config:	Pointer to the configuration structure for ADC0.
+  * @return status:     Initialization status
+  ************************************************************************************/
+ADC_DRV_State ADC0_DRV_Init(Adc0_config * adc0_config);
+
+/*************************************************************************************
+  * Function Name:		ADC0_DRV_Read
+  * Description:		Reads the ADC conversion result from ADC0.
+  * @param adc0_config:	Pointer to the configuration structure for ADC0.
+  * @return uint32_t:	ADC conversion result.
+  ************************************************************************************/
+uint32_t ADC0_DRV_Read(Adc0_config * adc0_config);
+
+#endif /* SOURCES_ADC_DRIVER_ADC_DRIVER_H_ */
